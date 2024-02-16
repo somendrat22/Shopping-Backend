7cccfa9a3b4dd77a8448a39190ac1b6f0fe66b90
Somendrat22 <somendrat22@gmail.com>
2024-02-16 21:51:52 +0530

    Get method added in the portaluser

--- src/main/java/com/shopping/database/api/Database/API/Controller/PortalUserController.java
+++ src/main/java/com/shopping/database/api/Database/API/Controller/PortalUserController.java
@@ -3,10 +3,9 @@ package com.shopping.database.api.Database.API.Controller;
 import com.shopping.database.api.Database.API.models.PortalUser;
 import com.shopping.database.api.Database.API.repository.PortalUserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/db/user")
@@ -20,4 +19,10 @@ public class PortalUserController {
         portalUserRepo.save(portalUser);
         return portalUser;
     }
+
+    @GetMapping("/getuser")
+    public PortalUser getUserById(@RequestParam UUID id){
+        return portalUserRepo.findById(id).orElse(null);
+    }
+
 }
